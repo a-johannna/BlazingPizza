f1caa62c10e7cde78b4d7506bb072c8abc566157
Andrea Johanna Villavicencio Lema <andrea.villavicenc@isepceu.es>
2025-02-12 09:24:41 +0100

    cambios

---
+++ BlazingPizza.Client/wwwroot/site.js
@@ -0,0 +1,7 @@
+﻿window.toggleDarkMode = (isDarkMode) => {
+    if (isDarkMode) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+};
